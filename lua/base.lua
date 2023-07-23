e4832206e2275848b2bf14c56832339ce28d8ec4
HANO Hiroyuki <onahdwahan@gmail.com>
2023-07-23 10:25:30 +0900

    add memo

--- lua/base.lua
+++ lua/base.lua
@@ -3,3 +3,6 @@ vim.opt.list = true
 vim.opt.tabstop = 4
 
 vim.cmd[[colorscheme hybrid]]
+
+vim.g.memolist_path = 'C:\\Users\\PCuser\\AppData\\Local\\nvim-data\\memo'
+
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 
 -- Easy write and exit
 
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>q', ':q<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>w', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>q', ':q<CR>', { noremap = true, silent = true })
 
 -- Quick load init.lua
 --vim.api.nvim_set_keymap('n', '<Leader>ri', ':<C-u>source $MYVIMRC<CR>', { noremap = true, silent = true })
@@ -16,5 +16,10 @@ vim.keymap.set('n', '<leader>tf', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<leader>tl', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>tb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>th', telescope_builtin.help_tags, {})
+
+-- memo
+vim.api.nvim_set_keymap('n', '<Leader>mn', ':MemoNew<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ml', ':MemoList<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>mg', ':MemoGrep<CR>', { noremap = true, silent = true })
 
 
